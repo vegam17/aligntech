52ae2d1bf49cd5eee145a0f6f4400faaa1a26b8f
Miguel Vega <vega.dev@outlook.com>
2023-06-09 11:33:44 -0400

    updated header and social components to pull data directly from component

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,12 @@
 import Header from './components/Header';
 import WorkExperience from './components/WorkExperience';
 import Container from './components/Container';
-import data from './data';
 import styles from './App.module.scss';
 
 export default function App() {
-  const { header } = data;
   return (
     <main>
-      <Header
-        title={header.title}
-        subtitle={header.subtitle}
-        tagline={header.tagline}
-        social={header.social}
-      />
+      <Header />
       <Container>
         <div className={styles.columns}>
           <WorkExperience />
--- src/data/index.ts
+++ src/data/index.ts
@@ -3,13 +3,13 @@ const data = {
     title: 'Miguel Vega-Herrera',
     subtitle: 'Software Engineer',
     tagline:
-      'Over 7 years of experience leading the design and development of scalable full-stack web-based solutions from initial concept to deployment',
-    social: [
-      { slug: 'linkedin', url: 'https://www.linkedin.com/in/vegaherreram/' },
-      { slug: 'email', url: 'mailto:miguel@vega.dev' },
-      { slug: 'phone', url: 'tel:+12525251053' }
-    ]
+      'Over 7 years of experience leading the design and development of scalable full-stack web-based solutions from initial concept to deployment'
   },
+  social: [
+    { slug: 'linkedin', url: 'https://www.linkedin.com/in/vegaherreram/' },
+    { slug: 'email', url: 'mailto:miguel@vega.dev' },
+    { slug: 'phone', url: 'tel:+12525251053' }
+  ],
   work: [
     {
       date: {
